ffbca5a2b59e90d8fe8571d04f14a0a05141c1b9
Mickaël Véril <mveril@irsamc.ups-tlse.fr>
2020-12-02 09:42:21 +0100

    Fix align with website structures

--- static/js/Geometry.js
+++ static/js/Geometry.js
@@ -11,8 +11,8 @@ class Geometry {
     for (const a of this.molecule.atoms) {
       var line =a.label.padEnd(4, ' ')
       line += a.x.toFixed(8).padStart(11,' ')
-      line += a.y.toFixed(8).padStart(21,` `)
-      line += a.z.toFixed(8).padStart(21,` `)
+      line += a.y.toFixed(8).padStart(22,` `)
+      line += a.z.toFixed(8).padStart(22,` `)
       lines.push(line)
     }
     return lines.join("\n")
